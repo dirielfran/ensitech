39140c2986613e463dac2c15b4f21de7134a23be
eareizathese <elvis.areiza@these.com.uy>
2023-12-07 20:45:38 -0300

    Se modifica contraseña de usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO `usuarios` VALUES (10,NULL,NULL,NULL,NULL,'Soporte','generico3@gmail.com',false,'Kiosco','$2a$10$QPJSZV.XAzPd1p/M0rqzPeOJRbaznP.uqej1vO0ukZ/6XKHuDS43u','kiosco'),(16,NULL,NULL,NULL,NULL,'areiza','dirielfran@gmail.com',true,'Elvis','$2a$10$QPJSZV.XAzPd1p/M0rqzPeOJRbaznP.uqej1vO0ukZ/6XKHuDS43u','eareiza');
+INSERT INTO `usuarios` VALUES (10,NULL,NULL,NULL,NULL,'Soporte','generico3@gmail.com',false,'ensitech','$2a$10$t..1wk3cpZ7QPT8K9kMo0eG0PMh16rCdXoiz.lR05xyJCR2NR8TEy','ensitech'),(16,NULL,NULL,NULL,NULL,'areiza','dirielfran@gmail.com',true,'Elvis','$2a$10$t..1wk3cpZ7QPT8K9kMo0eG0PMh16rCdXoiz.lR05xyJCR2NR8TEy','eareiza');
 INSERT INTO `roles` VALUES (2,'ROLE_ADMIN'),(3,'ROLE_COORDINATOR'),(1,'ROLE_USER');
 INSERT INTO `usuarios_roles` VALUES (10,1),(16,1),(10,2),(16,2),(10,3),(16,3);
